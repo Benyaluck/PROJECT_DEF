ecde94b2e3b1d88f7e5d36cc9890f29432f58fd9
patsawee <36643724+patsawee@users.noreply.github.com>
2018-04-11 13:52:30 +0700

    Add files via upload

---
+++ BunchOfCarrot.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BunchOfCarrot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BunchOfCarrot extends Actor
+{
+    /**
+     * Act - do whatever the BunchOfCarrot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
